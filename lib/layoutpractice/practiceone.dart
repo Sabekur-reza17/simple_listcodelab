faf2da8809bd792073941344dda8c7543742e775
Sabekur <sabekur2017@gmail.com>
2020-08-16 10:55:52 +0600

    complex ui

--- lib/layoutpractice/practiceone.dart
+++ lib/layoutpractice/practiceone.dart
@@ -48,17 +48,59 @@ Widget myLayoutWidget() {
 
     ],
   );*/
-  return Stack(
-    alignment: Alignment.bottomCenter,
+return Container(
+  margin: EdgeInsets.all(16.0),
+  padding: EdgeInsets.all(16.0),
+  decoration: BoxDecoration(
+    color: Colors.purple[900],
+    border: Border.all(),
+    borderRadius: BorderRadius.all(Radius.circular(3.0)),
+  ),
+  child: Column(
+    mainAxisSize: MainAxisSize.min,
     children: <Widget>[
-      Image.asset('assets/imageone.png'),
+      Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(Icons.favorite,color: Colors.green,),
+          ),
+          Text(
+            'BEAMS',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          )
+        ],
+      ),
       Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Stack example',
-          style: TextStyle(fontSize: 30),
+        padding: EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 0,
         ),
+        child: Text('Send programmable push notifications to iOS and Android devices with delivery and open rate tracking built in',
+        style: TextStyle(
+          color: Colors.white,
+        ),),
+
+      ),
+      Row(
+        children: <Widget>[
+          Text('Explore beams',
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Icon(Icons.arrow_forward,
+            color: Colors.green,),
+          )
+        ],
       )
     ],
-  );
+  ),
+
+);
+
 }
