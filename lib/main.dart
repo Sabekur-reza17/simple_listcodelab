6c61ead8ce0e0918fa30d4570234824e5b402b57
Sabekur <sabekur2017@gmail.com>
2020-08-18 19:48:27 +0600

    camera test

--- lib/main.dart
+++ lib/main.dart
@@ -17,4 +17,106 @@ import 'images/ImageFromNetwork.dart';
 /*void main() => runApp(MyLonglists(
   products: List<String>.generate(500, (i) => "Product List: $i"),
 ));*/
-void main() {runApp(ImageFromNetwork());}
+import 'dart:async';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras=await availableCameras();
+  final firstCamera=cameras.first;
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen (
+        camera:firstCamera,
+      ),
+    )
+  );
+}
+
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({Key key, this.camera}) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+
+}
+class TakePictureScreenState extends State<TakePictureScreen>{
+  CameraController _cameraController;
+  Future<void> _initialzeControllerFuture;
+
+  @override
+  void initState() {
+    _cameraController= CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+    _initialzeControllerFuture=_cameraController.initialize();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture'),),
+      body: FutureBuilder<void>(
+        future: _initialzeControllerFuture,
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            return CameraPreview(_cameraController);
+          }else{
+            return Center(child: CircularProgressIndicator(),);
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        onPressed: () async {
+          try{
+          await _initialzeControllerFuture;
+          final path =join(
+          (await getTemporaryDirectory()).path,
+          '${DateTime.now()}.png',
+          );
+          await _cameraController.takePicture(path);
+          Navigator.push(context,
+      MaterialPageRoute(
+      builder: (context) =>DisplayPictureScreen(imagePath:path),
+      ));
+      }catch(e){
+            print(e);
+      }
+      },
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the picture'),),
+      body: Image.file(File(imagePath)),
+    );
+    throw UnimplementedError();
+  }
+}
+
+/*void main() {runApp(ImageFromNetwork());}*/
